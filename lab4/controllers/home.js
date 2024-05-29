bc72a1bdaa2aa2bd327796fce7ebdf1d1f04dc9e
jahooba <josh.uapena20002@gmail.com>
2024-05-28 20:28:08 -0700

    Included mongoose remote database connection

--- lab4/controllers/home.js
+++ lab4/controllers/home.js
@@ -1,8 +1,3 @@
-// Controller handler to handle functionality in home page
-function getHome(request, response){
-  // do any work you need to do, then response.render('home', {title: 'home'});
-  
-  }
 // Example for handle a get request at '/' endpoint.
 
 function getHome(request, response){
--- lab4/server.js
+++ lab4/server.js
@@ -4,6 +4,25 @@ const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
 const path = require('path');
 
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://jpena079:PZ8J9sdjSquThjNe@cluster0.gz2bz2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+
 // import handlers
 const homeHandler = require('./controllers/home.js');
 const roomHandler = require('./controllers/room.js');
